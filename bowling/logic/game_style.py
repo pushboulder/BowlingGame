ffb058ec0e632f571a4d9504da142186750318e9
pushboulder <rob.ray.eng@gmail.com>
2021-04-06 04:39:45 -0700

    Added get_headers to GameStyle

--- bowling/logic/game_style.py
+++ bowling/logic/game_style.py
@@ -5,3 +5,11 @@ class GameStyle:
 
     def __init__(self, game_id=None):
         self.game_manager = GameManager(game_id)
+
+    @staticmethod
+    def get_headers():
+        headers = {}
+        for index in range(1, 12):
+            headers[index] = index
+        headers[11] = 'Total Score'
+        return headers
