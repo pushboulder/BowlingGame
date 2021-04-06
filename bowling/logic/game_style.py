39dc6dbd5488655929d7dab62bb654ff46c0b914
pushboulder <rob.ray.eng@gmail.com>
2021-04-06 06:00:22 -0700

    Added get_roll_results
    Added test to check partially completed games.

--- bowling/logic/game_style.py
+++ bowling/logic/game_style.py
@@ -13,3 +13,55 @@ class GameStyle:
             headers[index] = index
         headers[11] = 'Total Score'
         return headers
+
+    def get_roll_results(self):
+        roll_results = []
+        for name, frame in self.game_manager.frames.items():
+            frame_results = []
+            for pin_set in frame.pin_sets:
+                status = pin_set.get_status()
+                frame_results.extend(self.get_strike_results(status, name))
+                frame_results.extend(self.get_spare_results(status, pin_set))
+                frame_results.extend(self.get_normal_results(status, pin_set))
+            frame_results.extend(self.get_blank_results(name, len(frame_results)))
+            roll_results.extend(frame_results)
+
+        return roll_results
+
+    @staticmethod
+    def get_strike_results(status, name):
+        strike_result = []
+        if status == 'Strike':
+            if name != 10:
+                strike_result.append('')
+            strike_result.append('X')
+        return strike_result
+
+    @classmethod
+    def get_spare_results(cls, status, pin_set):
+        spare_results = []
+        if status == 'Spare':
+            roll_1 = cls.convert_zero_result(pin_set.rolls[0])
+            spare_results.extend([roll_1, '/'])
+        return spare_results
+
+    @staticmethod
+    def convert_zero_result(roll):
+        return str(roll) if roll != 0 else '-'
+
+    @classmethod
+    def get_normal_results(cls, status, pin_set):
+        normal_results = []
+        if status in ['Complete', 'Incomplete']:
+            for roll in pin_set.rolls:
+                normal_results.append(cls.convert_zero_result(roll))
+        return normal_results
+
+    @staticmethod
+    def get_blank_results(name, count):
+        blanks_list = []
+        required_results = 3 if name == 10 else 2
+        while count < required_results:
+            blanks_list.append('')
+            count += 1
+        return blanks_list
--- bowling/tests/test_game_style.py
+++ bowling/tests/test_game_style.py
@@ -11,13 +11,14 @@ class TestGameStyle(TestCase):
             7: 7, 8: 8, 9: 9, 10: 10, 11: 'Total Score'
         }
         cls.game_style = GameStyle()
-        pins_hit_list = [10, 7, 3, 9, 0, 10, 0, 8, 8, 2, 0, 6, 10, 10, 10, 8, 1]
-        for pins_hit in pins_hit_list:
+        cls.pins_hit_list = [10, 7, 3, 9, 0, 10, 0, 8, 8, 2, 0, 6, 10, 10, 10, 8, 1]
+        for pins_hit in cls.pins_hit_list:
             cls.game_style.game_manager.roll(pins_hit)
         cls.expected_roll_results = [
             '', 'X', '7', '/', '9', '-',
             '', 'X', '-', '8', '8', '/',
-            '-', '6', 'X', 'X', 'X', '8', '1'
+            '-', '6', '', 'X', '', 'X',
+            'X', '8', '1'
         ]
 
     def test_takes_game_id_creates_game_manager(self):
@@ -41,5 +42,28 @@ class TestGameStyle(TestCase):
     def test_get_roll_results_returns_expected_data(self):
         self.assertEqual(
             self.game_style.get_roll_results(),
-            self.expected_roll_results
+            self.expected_roll_results,
+            '\nExpected: {}\nActual: {}'.format(
+                self.game_style.get_roll_results(),
+                self.expected_roll_results
+            )
         )
+
+    def test_partial_game_results(self):
+        game_style = GameStyle()
+        pins_hit_list = self.pins_hit_list[0:9]
+        for pins_hit in pins_hit_list:
+            game_style.game_manager.roll(pins_hit)
+
+        roll_results = game_style.get_roll_results()
+        expected = self.expected_roll_results[0:11]
+        expected.extend([''] * 10)
+        self.assertEqual(
+            roll_results,
+            expected,
+            '\nExpected: {}\nActual:   {}'.format(
+                roll_results,
+                expected
+            )
+        )
+
