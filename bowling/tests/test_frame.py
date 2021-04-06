4b529491082337434a5dadd0f935e807067800b4
pushboulder <rob.ray.eng@gmail.com>
2021-04-05 21:48:35 -0700

    Test to ensure no more than two rolls per frame (3 for 10)

--- bowling/tests/test_frame.py
+++ bowling/tests/test_frame.py
@@ -9,12 +9,20 @@ class TestFrame(TestCase):
             'max_pin_sets': 1,
             'pin_sets': [],
             'score': 0,
-            'name': 1
+            'name': 1,
+            'remaining_rolls': 2
         }
         cls.max_pin_sets_data = {
             1: 1, 2: 1, 3: 1, 4: 1, 5: 1,
             6: 1, 7: 1, 8: 1, 9: 1, 10: 3
         }
+        cls.max_rolls_data = [
+            {'name': 1, 'rolls': [2, 8, 10, 10], 'pins_hit': [2, 8], 'pin_set_count': 1},
+            {'name': 10, 'rolls': [2, 8, 10, 10], 'pins_hit': [2, 8, 10], 'pin_set_count': 2},
+            {'name': 10, 'rolls': [10, 0, 10, 8], 'pins_hit': [10, 0, 10], 'pin_set_count': 2},
+            {'name': 10, 'rolls': [10, 10, 6, 4], 'pins_hit': [10, 10, 6], 'pin_set_count': 3},
+            {'name': 10, 'rolls': [2, 7, 6, 4], 'pins_hit': [2, 7], 'pin_set_count': 2}
+        ]
 
     def test_frame_takes_one_arg_has_expected_defaults(self):
         frame = Frame(self.defaults['name'])
@@ -23,7 +31,8 @@ class TestFrame(TestCase):
                 'max_pin_sets': frame.max_pin_sets,
                 'pin_sets': frame.pin_sets,
                 'score': frame.score,
-                'name': frame.name
+                'name': frame.name,
+                'max_rolls': frame.max_rolls
             },
             self.defaults
         )
@@ -44,4 +53,22 @@ class TestFrame(TestCase):
             self.assertEqual(
                 len(frame.pin_sets),
                 frame.max_pin_sets
+            )
+
+    def test_roll_method_has_remaining_rolls_increases_once_on_pin_set_add(self):
+        for data in self.max_rolls_data:
+            frame = Frame(data['name'])
+            for roll in data['rolls']:
+                frame.roll(roll)
+            self.assertEqual(
+                len(frame.pin_sets),
+                data['pin_set_count']
+            )
+            pins_hit = []
+            for pin_set in frame.pin_sets:
+                pins_hit.extend(pin_set.rolls)
+
+            self.assertEqual(
+                pins_hit,
+                data['pins_hit']
             )
