33f7949cbb57ee07da07b9bf765f8788bafd6ecd
pushboulder <rob.ray.eng@gmail.com>
2021-04-05 20:56:33 -0700

    Test to see max pin sets increases for Frame 10

--- bowling/tests/test_frame.py
+++ bowling/tests/test_frame.py
@@ -11,6 +11,10 @@ class TestFrame(TestCase):
             'score': 0,
             'name': 1
         }
+        cls.max_pin_sets_data = {
+            1: 1, 2: 1, 3: 1, 4: 1, 5: 1,
+            6: 1, 7: 1, 8: 1, 9: 1, 10: 3
+        }
 
     def test_frame_takes_one_arg_has_expected_defaults(self):
         frame = Frame(self.defaults['name'])
@@ -23,3 +27,11 @@ class TestFrame(TestCase):
             },
             self.defaults
         )
+
+    def test_max_pin_sets_changes_if_name_is_ten(self):
+        for name, max_pin_sets in self.max_pin_sets_data.items():
+            frame = Frame(name)
+            self.assertEqual(
+                frame.max_pin_sets,
+                max_pin_sets
+            )
