700c0eedbc585a24fcc75c7c41c5718fc603b6ed
pushboulder <rob.ray.eng@gmail.com>
2021-04-05 20:57:34 -0700

    Frame 10 now gets 3 pin sets

--- bowling/logic/frame.py
+++ bowling/logic/frame.py
@@ -1,6 +1,9 @@
 class Frame:
     def __init__(self, name):
-        self.max_pin_sets = 1
+        if name == 10:
+            self.max_pin_sets = 3
+        else:
+            self.max_pin_sets = 1
         self.pin_sets = []
         self.score = 0
         self.name = name
