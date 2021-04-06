a69c3944243e7fc084a14d23a884229565d2e5d5
pushboulder <rob.ray.eng@gmail.com>
2021-04-06 01:32:30 -0700

    Adding frame init to GameManager

--- bowling/logic/game_manager.py
+++ bowling/logic/game_manager.py
@@ -1,11 +1,14 @@
 from bowling.models.game import Game
+from bowling.logic.frame import Frame
 
 
 class GameManager:
     game = None
+    frames = {}
 
     def __init__(self, game_id=None):
         self.set_game(game_id)
+        self.set_frames()
 
     def set_game(self, game_id):
         if game_id is None:
@@ -15,3 +18,7 @@ class GameManager:
 
     def create_game(self):
         self.game = Game.objects.create()
+
+    def set_frames(self):
+        for index in range(1, 11):
+            self.frames[index] = Frame(index)
